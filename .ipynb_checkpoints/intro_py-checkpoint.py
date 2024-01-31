da996ade66f2443f07d8a1c3d4d63e4f0574452a
MuhammadBabarAi99 <muhammadbabarict1@gmail.com>
2024-01-31 10:19:38 -0800

    add variable & data types

---
+++ .ipynb_checkpoints/intro_py-checkpoint.py
@@ -0,0 +1,29 @@
+#what is programming?
+#programming is the process of creating a set 
+# of instructions that tell a computer how to perform a task.
+# These instructions can be written in a number of different "languages",
+# or which are simply different ways of organizing the instructions and text.
+# The set of instructions is often called a program or a script.
+# The computer follows the instructions in the program one after the other, 
+# until the end.
+# The order of the instructions is crucial to the computer,
+# and they are executed in a very literal manner.   
+# Programming is a very creative and rewarding activity,
+# but it can also be frustrating and time-consuming.
+# If you have ever spent hours trying to find a missing semicolon,
+# you will know what I mean.
+# The computer will do exactly what you tell it to do.
+#what is python?
+#Python is a programming language.
+#Python can be used on a server to create web applications.
+#Python can be used alongside software to create workflows.
+#Python can connect to database systems. It can also read and modify files.
+#Python can be used to handle big data and perform complex mathematics.
+#Python can be used for rapid prototyping, or for production-ready software development.
+#why python?
+#Python works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc).  
+#Python has a simple syntax similar to the English language.
+#Python has syntax that allows developers to write programs with fewer lines than some other programming languages.
+#Python runs on an interpreter system, meaning that code can be 
+# executed as soon as it is written.
+#This means that prototyping can be very quick.
---
+++ intro_py.py
@@ -0,0 +1,57 @@
+#what is programming?
+#programming is the process of creating a set 
+# of instructions that tell a computer how to perform a task.
+# These instructions can be written in a number of different "languages",
+# or which are simply different ways of organizing the instructions and text.
+# The set of instructions is often called a program or a script.
+# The computer follows the instructions in the program one after the other, 
+# until the end.
+# The order of the instructions is crucial to the computer,
+# and they are executed in a very literal manner.   
+# Programming is a very creative and rewarding activity,
+# but it can also be frustrating and time-consuming.
+# If you have ever spent hours trying to find a missing semicolon,
+# you will know what I mean.
+# The computer will do exactly what you tell it to do.
+print ("hello world")
+########################
+# Pyton Variable and Data Type ?
+# Python has the following data types built-in by default, in these categories:
+# expalin each datatypes with all function and  examples 
+  
+
+
+# Text Type:	str
+# string is 
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# Getting the Data Type 
+# what is getting the data type ?
+
+# You can get the data type of any object by using the type() function:
+# Example
+# Print the data type of the variable x:
+x = 5
+print(type(x))
+# Setting the Data Type
+# In Python, the data type is set when you assign a value to a variable:
+# Example
+# Setting the data type of a variable
+x = "Hello World"	# str
+x = 20	# int
+x = 20.5	# float
+x = 1j	# complex
+x = ["apple", "banana", "cherry"]	# list
+x = ("apple", "banana", "cherry")	# tuple
+x = range(6)	# range
+x = {"name" : "John", "age" : 36}	# dict
+x = {"apple", "banana", "cherry"}	# set
+x = frozenset({"apple", "banana", "cherry"})	# frozenset
+x = True	# bool
+x = b"Hello"	# bytes
+x = bytearray(5)	# bytearray
+x = memoryview(bytes(5))	# memoryview
