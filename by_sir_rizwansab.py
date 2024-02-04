1cf7a537891fe82342d7c7403a6cbcf03cdbd282
babarai45 <muhammadbabarai45@gmail.com>
2024-02-03 18:28:35 -0800

    numeric datatypes

---
+++ by_sir_rizwansab.py
@@ -0,0 +1,68 @@
+# here is all content according to sir rizwan sab for learning python from scratch to advance level 
+# and become a professional python developer    
+# 1. Introduction to Python
+
+# 2. Variables and Data Types
+# types of data types in python
+# 1. numeric data type (int, float, complex)
+ # int data type (int)
+# int is a whole number, positive or negative,  without decimals, of unlimited length.
+# # it default value is 0
+# example:
+x=int()  # it default value is 0
+print(x) # 0
+a,*b=10,20,40,50
+print(a) # 10
+print(b) # [20, 40, 50] # *b is a list
+
+# float data type (float)
+# float is a number, positive or negative, containing one or more decimals.
+# float data type can be represented in three ways
+# it default value is 0.0
+# example:
+x=float()  # it default value is 0.0
+print(x) # 0.0
+a,*b=10.5,20.5,40.5,50.5
+print(a) # 10.5
+print(b) # [20.5, 40.5, 50.5] # *b is a list
+
+
+# float is a number, positive or negative, containing one or more decimals.
+# 2. string data type (str) 
+# A string in Python is a sequence of characters. It is a derived data type. Strings are immutable. 
+# This means that once defined, they cannot be changed.
+# 3. sequence data type (list, tuple, range)
+# 4. mapping data type (dict)
+# 5. set data type (set, frozenset)
+# 6. boolean data type (bool)
+# 7. binary data type (bytes, bytearray, memoryview)
+# 8. None data type (NoneType)   
+# 3. Operators
+# 4. Conditional Statements
+# 5. Looping
+# 6. Functions
+# 7. File Handling
+# 8. Exception Handling
+# 9. Modules
+# 10. Classes and Objects
+# 11. Regular Expressions
+# 12. Multithreading
+# 13. Networking
+# 14. Database Access
+# 15. Python GUI
+# 16. Python XML Processing
+# 17. Python Web Scraping
+# 18. Python Data Science
+# 19. Python Machine Learning
+# 20. Python Deep Learning
+# 21. Python Robotics
+# 22. Python Game Development
+# 23. Python Web Development
+# 24. Python Django Framework
+# 25. Python Flask Framework
+# 26. Python Pyramid Framework
+# 27. Python Bottle Framework
+# 28. Python CherryPy Framework
+# 29. Python Web2Py Framewor
+###############################################only chalanges not answer#####################33333333
+# python numeric datatype challenges python  problems 40 complex problems only include related just int ,float,bool,complex each of 10 ? 
